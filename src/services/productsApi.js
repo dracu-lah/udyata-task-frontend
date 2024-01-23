f38302c7194e1239b51e882ba873792a97c10a5f
dracu-lah <nevilnicks4321@gmail.com>
2024-01-23 15:37:34 +0530

    moved base url to env

--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+const baseUrl = import.meta.env.VITE_BASE_URL;
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://dummyjson.com",
+    baseUrl,
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
